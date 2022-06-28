ed3473aa38bd425bef200716aa05cbb444e54d9f
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-06-28 20:40:26 +0530

    lev 2 encryption

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const ejs= require("ejs");
 const app = express();
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -11,10 +12,13 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
-const userSchema ={
+const userSchema =new mongoose.Schema({
   email: String,
   password: String
-};
+});
+const secret = "THisisourlittlesecret.";
+userSchema.plugin(encrypt,{secret: secret, encryptedFields:['password']});
+
 const User = new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
